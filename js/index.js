76693e66dda4cf4fb2f3abc6eae9d3381dfac472
Valeria Meza <valeriameza173@gmail.com>
2020-04-19 22:37:21 -0500

    added a konami code feature

---
+++ js/index.js
@@ -0,0 +1,21 @@
+(function () {
+  "use strict";
+  $(document).ready(function () {
+    console.log("Looking Good");
+    var superMarioTrigger = "38384040373937396665";
+    var triggerMario = "";
+    $(document).keyup(function (e) {
+      console.log(e.which);
+      triggerMario += e.which;
+
+      if (triggerMario === superMarioTrigger) {
+        console.log("Here we Gooooo!");
+        $("#mario").prepend('<img id="marioDrive" src="images/mario.png" />');
+        $("#mario").prepend('<img id="luigiDrive" src="images/luigi.png" />');
+        setTimeout(function () {
+          $("#mario").empty();
+        }, 5000);
+      }
+    });
+  });
+})();
